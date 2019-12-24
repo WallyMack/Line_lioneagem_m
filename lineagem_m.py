40e40d981f16f0c6451a6310c80f7cf038b9b61b
Ｍack.Chuang <>
2019-12-24 12:12:25 +0800

    no message

--- lineagem_m.py
+++ lineagem_m.py
@@ -61,7 +61,7 @@ def handle_message(event):
         print(result)
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=str(result))
+            TextSendMessage(text=str(result)))
 
 
 if __name__ == "__main__":
